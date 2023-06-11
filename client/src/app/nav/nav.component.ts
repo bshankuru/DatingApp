f2ae04220a15587ab10f88d02253d085732da481
bshankuru <bijoyshankuru@gmail.com>
2023-06-11 14:10:20 -0400

    End of section 7

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -34,8 +34,8 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(
       {
-        next: _ => this.router.navigateByUrl('/members'),
-        error: error => this.toastr.error(error.error)
+        next: _ => this.router.navigateByUrl('/members')
+       
       }
     );
   }
