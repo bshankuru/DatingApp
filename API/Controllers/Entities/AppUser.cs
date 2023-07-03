9369c60ee3e8e63bab340e082eed45e6b863f973
bshankuru <bijoyshankuru@gmail.com>
2023-07-03 13:14:10 -0400

    end of section 9

--- API/Controllers/Entities/AppUser.cs
+++ API/Controllers/Entities/AppUser.cs
@@ -14,6 +14,8 @@ namespace API.Controllers.Entities
         public DateOnly DateOfBirth { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
+
+
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
